811bd0e44601d817e832a23f012dca89cb6fbabd
Vladimir Podolyan <vpodolyan@outlook.com>
2020-10-11 15:45:18 +0300

    Add displaying symbols in the dropdown + more styles + click outside

--- src/components/StockSearchInput.svelte
+++ src/components/StockSearchInput.svelte
@@ -1,14 +1,22 @@
 
 <script lang="ts">
+    import {clickOutside} from '../helpers/clickOutside';
+
     import {stocksApi} from '../api/stocks';
     import type { IStockSearchInfo } from '../api/stocks/types/IStockSearchInfo';
 
     import { stocksData } from '../stores';
 
+    let showResults = false;
     let result: IStockSearchInfo[] = [];
 
     async function handleChange({target: { value }}) {
         result = await stocksApi.search(value);
+        showResults = true;
+    }
+
+    function handleClickOutside() {
+        showResults = false;
     }
 
     async function onStockClick(stock: IStockSearchInfo) {
@@ -16,22 +24,27 @@
             const result = await stocksApi.getQuoteInfo(stock.symbol);
             stocksData.update(data => ({...data, children: [...data.children, {name: stock.symbol, value: +result.price}]})) 
         } catch {}
+        finally {
+            showResults = false;
+        }
     }
 </script>
 
 <div class='wrapper'>
     <div class="container">
         <input on:change={handleChange} />
-        <div class='search-results'>
-            {#each result as stock}
-                <div
-                    class='search-results__item'
-                    on:click={() => onStockClick(stock)}
-                >
-                    {stock.name}
-                </div>
-            {/each}
-        </div>
+        {#if showResults}
+            <div class='search-results' use:clickOutside on:clickoutside={handleClickOutside} >
+                {#each result as stock}
+                    <div
+                        class='search-results__item'
+                        on:click={() => onStockClick(stock)}
+                    >
+                        {stock.name}<span class='search-results__item__symbol'>{stock.symbol}</span>
+                    </div>
+                {/each}
+            </div>
+        {/if}
     </div>
 </div>
 
@@ -56,19 +69,30 @@
         color: #ff3e00;
         font-size: 1.5em;
         font-weight: 100;
+        line-height: 2.5em;
     }
 
     .search-results {
+        position: absolute;
         display: flex;
         flex-direction: column;
         align-items: flex-start;
         padding: 0 32px;
+        box-shadow: 0px 3px 7px 0px rgba(50, 50, 50, 0.55);
+        background: white;
+        z-index: 100;
     }
 
     .search-results__item {
         transition: color 0.2s;
         width: 100%;
         text-align: left;
+    }
+
+    .search-results__item__symbol {
+        font-size: 0.6em;
+        vertical-align: super;
+        padding-left: 4px;
     }
 
     .search-results__item:hover {
